88cc78c3212de8be8975a89779b5e4fe015c1fff
AlexeyChudinoff <201900@mail.ru>
2024-09-16 10:47:38 +0700

    создали ветку

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Создали ветку для домашки 3");
     }
 }
